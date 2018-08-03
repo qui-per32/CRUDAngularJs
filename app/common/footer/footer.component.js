9d5678e1c61d37adc3f77cb40811c053eb371844
Enrique <qui.per.gonz32@gmail.com>
2018-08-03 18:05:32 +0200

    modified design

--- app/common/footer/footer.component.js
+++ app/common/footer/footer.component.js
@@ -3,11 +3,14 @@ export const FooterComponent ={
 
 <div class="container-fluid footerApp">
     <div class="row">
-        <div class="col-12">
+        <div class="col-12 justify-content-between">
             <nav class="navbar navbar-dark bg-dark text-white fixed-bottom">
                 <p>
                     © 2018 Copyright: Alfatec
                 </p>
+                    <button type="button" class="btn btn-sm btn-primary">
+                        <a ui-sref="home" class="nav-link text-white">Home</a>
+                    </button>
             </nav>
         </div>
     </div>

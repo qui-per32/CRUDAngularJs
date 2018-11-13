793a280913e57bc77be3b09faea5301894d1c853
Enrique <qui.per.gonz32@gmail.com>
2018-11-13 18:23:25 +0100

    Updates

--- app/app.component.js
+++
@@ -1,9 +0,0 @@
-export const AppComponent={
-    template:`
-       <div class="app">
-             <c-header></c-header>
-             <div ui-view></div>
-             <c-footer></c-footer>   
-       </div> 
-`
-}
--- app/components/components.js
+++
@@ -1,8 +0,0 @@
-import angular from 'angular'
-import home from './home'
-
-const components = angular
-.module('app.components',[home])
-.name
-
-export default components
